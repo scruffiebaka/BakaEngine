9c71b7338a265f6b3eacfda0caa54385c771ed86
scruffiebaka <itsmew2468@gmail.com>
2025-04-08 00:54:46 +0530

    gitignore stuff

--- BakaEngine.Core/obj/Debug/net9.0/BakaEngine.Core.AssemblyInfo.cs
+++ BakaEngine.Core/obj/Debug/net9.0/BakaEngine.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BakaEngine.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed5b33f7bda5420e3170471583769cdae3bdd9f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BakaEngine.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BakaEngine.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
